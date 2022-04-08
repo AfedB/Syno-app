29491e6fbb184460bde2f65854c95a704826b562
AfedB <hafedh@kabaun.com>
2022-04-08 02:44:12 +0200

    teste

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 function submitButton() {
   const apikey = "CgzBVTXbtz2kQOmVlhqgpFBrEp3omM5a";
   const element = document.getElementById("text-area");
@@ -6,7 +5,7 @@ function submitButton() {
   const url =
     "https://api.dicolink.com/v1/mot/" +
     word +
-    "/synonymes?limite=5&api_key=" +
+    "/synonymes?limite=3&api_key=" +
     apikey;
 
   fetch(url)
@@ -16,29 +15,10 @@ function submitButton() {
 
       for (var i = 0; i < data.length; i++) {
         element.innerHTML =
-          data[0].mot +
-          ", " +
-          data[1].mot +
-          ", " +
-          data[2].mot +
-          ", " +
-          data[3].mot +
-          ", " +
-          data[4].mot +
-          ".";
+          data[0].mot + ", " + data[1].mot + ", " + data[2].mot + ", ";
       }
     });
 }
-
-// document.getElementById("text").value = "";
-// document.getElementById("text-area").value = "";
-
-// var input = document.querySelector('#delete');
-// var textarea = document.querySelector('#text');
-
-// input.addEventListener('click', function () {
-//     textarea.value = '';
-// }, true);
 
 function deleteText() {
   let text = document.getElementById("text");
